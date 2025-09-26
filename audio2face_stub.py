4aaac674312b1dfd48d0616f0b3406f8879dbcd2
Billy Lynch <87466682+lynchbilly@users.noreply.github.com>
2025-09-25 22:00:48 -0500

    Add audio2face_stub.py with sync_avatar_with_audio function

---
+++ audio2face_stub.py
@@ -0,0 +1,4 @@
+def sync_avatar_with_audio(avatar_path, audio_path):
+    # This is a stub. In production, you would use the NVIDIA Audio2Face API.
+    # For now, we'll simulate a response.
+    return "Cartoon avatar lips synced to audio! (stubbed)"
